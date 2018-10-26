eb239633548a6caffbdef543f8eea2be4c5948a2
renatoliv6 <renato_oliv6@hotmail.com>
2018-10-26 12:07:28 +0100

    new

--- trabalhoso2/header_files/server-defaults.h
+++ trabalhoso2/header_files/server-defaults.h
@@ -3,45 +3,44 @@
 #define MEDIT_MAXUSERS 5
 
 
+// Devolve 0 ou 1 se user existe ou não
+int userCheck(char path[], char username[]){
 
-void loadDB(char path[], char username[]){
+    char *line= NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+    FILE *fd;
+    if ((fd = fopen(path, "r")) == NULL){
+        printf("Erro ao abrir ficheiro\n");
+        exit(1);
+    }
 
-  char *line= NULL;
-  size_t len = 0;
-  ssize_t read;
+    while((read = getline(&line,&len, fd)) != -1){
+        if((strcmp(line,username)) == 0){ 
+            return 1;
+        }
+    }
 
-  FILE *fd;
-  if ((fd = fopen(path, "r")) == NULL){
-     printf("Erro ao abrir ficheiro\n");
-     exit(1);
-  }
-
-  while((read = getline(&line,&len, fd)) != -1){
-     if((strcmp("renato","renato")) == 0){
-     	printf("tamanho linha: %d\n", read); 
-     
-     	printf("linha: %s",line);
-     }
-  }
-
-  free(line);
-  fclose(fd);
-  exit(EXIT_SUCCESS);
+    free(line);
+    fclose(fd);
+    return 0;
+    //exit(EXIT_SUCCESS);
 };
-
-int userCheck(char username){
-   
-}
 
 
 typedef struct{
    int nlinhas;
    int ncolunas;
-   int matriz[0][0];
-   char BDname[20];
+   char BDname[10];
    int nMaxUsers;
    int nPipeInt;
    char nPipepri;
 }SETTINGS;
 
-
+typedef struct{
+    int pid;
+    char fifo[20];
+    char user[10];
+    int posy; //guardar posicao onde se encontra user, está a ser usada linha
+}USER;
